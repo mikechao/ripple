06d826ade11d472a653e546efd15da30e821ac87
mike_chao <mike_chao@calypso.com>
2018-03-21 15:52:39 -0700

    Additional tests for server
    Change-Id: I7c24a9812990eb1a750781ad6877ed4bb60ab295

--- server/src/test/java/com/mike/TrustlineTest.java
+++ server/src/test/java/com/mike/TrustlineTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -28,6 +29,7 @@ import org.springframework.web.client.RestClientException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mike.config.ActiveMQBrokerConfig;
+import com.mike.exception.UserNotFoundException;
 import com.mike.exception.UsersAreSameException;
 import com.mike.service.UserService;
 import com.mike.startup.Main;
@@ -77,16 +79,12 @@ public class TrustlineTest {
     public void testTransactionWithSameUser() throws JsonProcessingException {
         Transaction sameUser = new Transaction(bob, bob, new Amount(10.0, "USD"));
         
-        String json = mapper.writeValueAsString(sameUser);
-        
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> entity = new HttpEntity<>(json, headers);
-        String sendUrl = getSendURL();
+        HttpEntity<String> entity = createEntity(sameUser);
         
         when(this.mockUserService.userExists(bob)).thenReturn(Boolean.TRUE);
+        
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(sendUrl, entity, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(getSendURL(), entity, String.class);
             UsersAreSameException exception = new UsersAreSameException(sameUser);
             String body = response.getBody();
             assertEquals("The error messages did NOT match", exception.getMessage(), body);
@@ -94,5 +92,73 @@ public class TrustlineTest {
             logger.error("Failed to handle a transaction with same users", e);
             fail("Failed to handle a transaction with same users" + e.getMessage());
         }
+    }
+    
+    @Test
+    public void testUserNotFound() throws JsonProcessingException {
+        Transaction notFound = new Transaction(bob, alice, new Amount(100.0, "USD"));
+        
+        HttpEntity<String> entity = createEntity(notFound);
+        
+        when(this.mockUserService.userExists(bob)).thenReturn(Boolean.TRUE);
+        when(this.mockUserService.userExists(alice)).thenReturn(Boolean.FALSE);
+        
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(getSendURL(), entity, String.class);
+            String body = response.getBody();
+            
+            UserNotFoundException exception = new UserNotFoundException(alice, notFound);
+            assertEquals("The error message did NOT match", exception.getMessage(), body);
+        } catch (Throwable t) {
+            logger.error("Failed to handle a transaction where user does not exist", t);
+            fail("Failed to handle a transaction where user does not exist " + t.getMessage());
+        }
+    }
+    
+    @Test
+    public void testRegularFromTransaction() throws JsonProcessingException {
+        Transaction transaction = new Transaction(bob, alice, new Amount(1000.0, "USD"));
+        
+        HttpEntity<String> entity = createEntity(transaction);
+        
+        when(this.mockUserService.userExists(bob)).thenReturn(Boolean.TRUE);
+        when(this.mockUserService.userExists(alice)).thenReturn(Boolean.TRUE);
+        when(this.mockUserService.getCurrentUser()).thenReturn(bob);
+
+        try {
+            ResponseEntity<?> response = restTemplate.postForEntity(getSendURL(), entity, ResponseEntity.class);
+            assertEquals("The http status did NOT match", HttpStatus.ACCEPTED, response.getStatusCode());
+        } catch (Throwable t) {
+            logger.error("Failed to process a correct transaction", t);
+            fail("Failed to handle a transaction where user does not exist " + t.getMessage());
+        }
+        
+    }
+    
+    @Test
+    public void testRegularToTransaction() throws JsonProcessingException {
+        Transaction transaction = new Transaction(alice, bob, new Amount(1000.0, "USD"));
+        
+        HttpEntity<String> entity = createEntity(transaction);
+        
+        when(this.mockUserService.userExists(bob)).thenReturn(Boolean.TRUE);
+        when(this.mockUserService.userExists(alice)).thenReturn(Boolean.TRUE);
+        when(this.mockUserService.getCurrentUser()).thenReturn(bob);
+
+        try {
+            ResponseEntity<?> response = restTemplate.postForEntity(getSendURL(), entity, ResponseEntity.class);
+            assertEquals("The http status did NOT match", HttpStatus.ACCEPTED, response.getStatusCode());
+        } catch (Throwable t) {
+            logger.error("Failed to process a correct transaction", t);
+            fail("Failed to handle a transaction where user does not exist " + t.getMessage());
+        }
+        
+    }
+    
+    private HttpEntity<String> createEntity(Transaction transaction) throws JsonProcessingException {
+        String json = mapper.writeValueAsString(transaction);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(json, headers);
     }
 }
